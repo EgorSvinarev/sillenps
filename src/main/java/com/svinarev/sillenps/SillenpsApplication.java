a11cec39e64e79dc3687fba733369c8d0a91b313
Egor Svinarev <egor-rdd@mail.ru>
2021-06-13 09:46:57 +0300

    Init commit.

---
+++ src/main/java/com/svinarev/sillenps/SillenpsApplication.java
@@ -0,0 +1,13 @@
+package com.svinarev.sillenps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SillenpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SillenpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svinarev/sillenps/SillenpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svinarev.sillenps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SillenpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
