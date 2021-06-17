4be825fdd01d2ce87c171c6ceb1495f96faf798f
Egor Svinarev <egor-rdd@mail.ru>
2021-06-17 14:20:52 +0300

    Data access layer for a User entity was implemented.

---
+++ src/main/java/com/svinarev/sillenps/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.svinarev.sillenps.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.svinarev.sillenps.entities.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
