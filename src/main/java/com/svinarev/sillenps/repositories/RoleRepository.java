ef228877afd5de85a44e2166f2fc0817e069396f
Egor Svinarev <egor-rdd@mail.ru>
2021-06-17 14:21:05 +0300

    Data access layer for a Role entity was implemented.

---
+++ src/main/java/com/svinarev/sillenps/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.svinarev.sillenps.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.svinarev.sillenps.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer>{
+
+}
