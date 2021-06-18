970cc739ab521a196f2d763470f2cad94f24c0a6
Egor Svinarev <egor-rdd@mail.ru>
2021-06-18 12:37:47 +0300

    Extended repository functionality.

--- src/main/java/com/svinarev/sillenps/repositories/RoleRepository.java
+++ src/main/java/com/svinarev/sillenps/repositories/RoleRepository.java
@@ -1,9 +1,11 @@
 package com.svinarev.sillenps.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.svinarev.sillenps.entities.Role;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer>{
 
 }
--- src/main/java/com/svinarev/sillenps/repositories/UserRepository.java
+++ src/main/java/com/svinarev/sillenps/repositories/UserRepository.java
@@ -1,11 +1,18 @@
 package com.svinarev.sillenps.repositories;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.svinarev.sillenps.entities.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-	Optional<User> findByUsernameAndEmail(String username, String email);
+	
+	Optional<User> findByUsername(String username);
+	
+	Optional<User> findByEmail(String username);
+	
+	Optional<User> findByUsernameOrEmail(String username, String email);
 }
